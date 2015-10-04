f4c528ceecf04585932eb373ae883b040ae4d7cf
Mouse Reeve <mouse.reeve@gmail.com>
2015-10-04 12:11:20 -0700

    Custom traversal
    No need to get every possible outcome

--- HoroscopeGenerator.py
+++ HoroscopeGenerator.py
@@ -1,13 +1,28 @@
 ''' Generate horoscopes '''
-from nltk.parse.generate import generate
+from nltk.grammar import Nonterminal
 from nltk import CFG
 import random
 
 grammar = CFG.fromstring(open('grammar.txt').read())
 
-sentences = []
-for sentence in generate(grammar, depth=5):
-    sentences.append(' '.join(sentence))
+def get_sentence(start=None, depth=7):
+    start = start if start else grammar.start()
 
-for i in range(25):
-    print(random.choice(sentences))
+    if isinstance(start, Nonterminal):
+        productions = grammar.productions(start)
+        if not depth:
+            # time to break the cycle
+            terminals = [p for p in productions if not isinstance(start, Nonterminal)]
+            if len(terminals):
+                production = terminals
+        production = random.choice(productions)
+
+        sentence = []
+        for piece in production.rhs():
+            sentence += get_sentence(start=piece, depth=depth-1)
+        return sentence
+    else:
+        return [start]
+
+for i in range(10):
+    print ' '.join(get_sentence(depth=10))
